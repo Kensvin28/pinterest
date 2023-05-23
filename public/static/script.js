6ba4c4479e2d487abee4fb91a8679d5766d063f8
kensvin <kensvin28@gmail.com>
2023-05-23 15:59:23 +0800

    additional image

--- public/static/script.js
+++ public/static/script.js
@@ -12,6 +12,16 @@ let images = [
     img_url: "https://i.ytimg.com/vi/SKNCfiLZigI/maxresdefault.jpg",
     alt_text: "Chilling Room",
   },
+  {
+    img_url:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc.pxhere.com%2Fphotos%2Fec%2F52%2Fgin_and_tonic_drink_happy_hour_grunge_glass_gin_alcohol_fresh-778213.jpg!d&f=1&nofb=1&ipt=2b7f486f1f9fc7054e9e3f2b1e5873975058dc6d774923452f405d1f4c67b817&ipo=images",
+    alt_text: "Chilling Drink",
+  },
+  {
+    img_url:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2015%2F05%2F09%2F14%2F31%2Fnature-759768_640.jpg&f=1&nofb=1&ipt=14b78096ade2e8ae252643a4f564c6cb553150ce4d6ccd954d6300ae04ee27e2&ipo=images",
+    alt_text: "Chilling Guys",
+  },
 ];
 
 function addImages(image, description) {
